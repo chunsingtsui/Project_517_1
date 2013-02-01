ce4008895296dbe0aeb93d2147ee387ee1143b35
jrwills2 <jrwills2@ncsu.edu>
2013-01-31 21:40:27 -0500

    initial commit

---
+++ Project_517_1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project5171::Application.config.secret_token = '7751985b8cc09a766ce9902806781606f4deb97699b59c42d6e6f9dcd4f25217a6f24f5edd80551abe516d456f81217488246e55d25fbee31cb86a68b2778ec8'
