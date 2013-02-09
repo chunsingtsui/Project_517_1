06b4a87cfca065179f31125aabc5c4530434218c
Chun Sing Tsui <ctsui@ncsu.edu>
2013-02-08 21:58:49 -0500

    Refactored some code in Views

--- Project_517_1/app/models/post.rb
+++ Project_517_1/app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ActiveRecord::Base
   validates_presence_of :category_id
   validates_presence_of :user_id
 
+
   def depth
     if self.post_id != nil
       return 1 + Post.find(self.post_id).depth
@@ -13,14 +14,30 @@ class Post < ActiveRecord::Base
     end
   end
 
+
   def getAllChildrenPosts
     Post.find_all_by_post_id(self.id, :order => "updated_at DESC")
   end
+
 
   def hasChildrenPost
     if Post.find_all_by_post_id(self.id).count() > 0
       true
     end
+  end
+
+
+  def creatorName
+    User.find(self.user_id).username
+  end
+
+
+  def category
+    Category.find(self.category_id).name
+  end
+
+  def numberOfVotes
+    Vote.find_all_by_post_id(self.id).count()
   end
 
 end
